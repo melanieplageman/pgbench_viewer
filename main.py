29f40e86d6cc01f0ae0a5323eecc41832815a543
Melanie Plageman <melanieplageman@gmail.com>
2023-09-20 16:32:34 -0400

    update main

--- main.py
+++ main.py
@@ -5,17 +5,20 @@ from source import *
 class TestLoader(Loader):
     buffercache_progress = PSQLSource()
     relfrozenxid_progress = PSQLSource()
-    aggwaits = PSQLSource()
+    waits = PgStatActivitySource()
     pg_stat_wal_progress = PSQLSource(parse_dates=['ts', 'stats_reset'])
-    pg_stat_io_progress = PSQLSource(parse_dates=['ts', 'stats_reset'])
+    pg_stat_io_progress = MultiPSQLSource(pivot_col=['backend_type', 'context',
+                                                     'object'],
+                                          parse_dates=['ts', 'stats_reset'],
+                                          plot_values=['writes'])
 
     pgbench_run_progress = PGBenchRunProgressSource()
     main = MainJSONSource(prefix='')
     iostat = IOStatSource()
+    read_exec_reports = ExecutionReportsSource(log_prefix='pgbench_log_read*')
 
 
-df = TestLoader.load('/mnt/force/pgresults/A/tmp.84G0vTOri4')
 
-print(df)
-print("Index:", df.index.dtype)
-print(df.dtypes)
+df1 = TestLoader.load('/mnt/force/pgresults/A/tmp.GxDQXTYOYB')
+df2 = TestLoader.load('/mnt/force/pgresults/A/tmp.U4pemqW1Eu')
+df3 = TestLoader.load('/mnt/force/pgresults/A/tmp.ZSAZK4dScx')
